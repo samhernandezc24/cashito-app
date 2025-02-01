75e0c86b235db7ea1beb160fb9447f33959c6ea9
samhernandezc24 <samd.hernandezc24@gmail.com>
2025-02-01 16:48:36 -0600

    First commit

---
+++ android/app/src/main/kotlin/com/example/cashito_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cashito_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:cashito_app/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
